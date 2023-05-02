1c6758f90ab67ef8df992b349862dc35db059ecd
Ravi <rathee.ravi26@gmail.com>
2023-05-02 10:53:10 +0200

    Update change3.py

--- change3.py
+++ change3.py
@@ -7,47 +7,29 @@ openai.api_key = "YOUR_API_KEY"
 # Choose an OpenAI language model
 model_engine = "davinci"  # or another model of your choice
 
-# Define some custom CSS styling for the search bar
-st.markdown(
-    """
-    <style>
-    .stTextInput > input {
-        padding-right: 120px;
-    }
-    .stTextInput > div {
-        position: absolute;
-        right: 0;
-        top: 100%;
-        width: 100%;
-        max-height: 200px;
-        overflow-y: auto;
-        z-index: 10;
-        background-color: white;
-        border: 1px solid #d3d3d3;
-        border-radius: 4px;
-        box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
-    }
-    </style>
-    """,
-    unsafe_allow_html=True,
-)
-
-# Create a Streamlit search bar widget with autocomplete
-search_input = st.text_input("Search", "")
-
-if search_input:
-    # Generate completions using the OpenAI language model
+# Define a function to generate suggestions using the OpenAI language model
+def get_suggestions(prompt):
     completions = openai.Completion.create(
         engine=model_engine,
-        prompt=search_input,
+        prompt=prompt,
         max_tokens=50,
         n=5,
         stop=None,
         temperature=0.5,
     )
+    return [choice.text for choice in completions.choices]
 
-    # Extract the text of each completion and format as a string
-    suggestions = "\n".join([choice.text for choice in completions.choices])
+# Create a Streamlit search bar widget with autocomplete
+search_input = st.text_input("Search", "")
 
-    # Display the suggestions below the search bar
-    st.markdown(f"<div>{suggestions}</div>", unsafe_allow_html=True)
+# Display suggestions in a dropdown menu as the user types
+if search_input:
+    suggestions = get_suggestions(search_input)
+    if suggestions:
+        st.write("Did you mean:")
+        selected_suggestion = st.selectbox("", suggestions, index=0)
+        search_input = selected_suggestion
+
+# Display the final search query after any suggestions have been selected
+if search_input:
+    st.write(f"Searching for: {search_input}")
